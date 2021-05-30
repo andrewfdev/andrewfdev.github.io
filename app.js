adaaeb50232df19255469b0acd245ddb0445f69d
andrewfdev <67811550+andrewfdev@users.noreply.github.com>
2021-05-30 12:13:44 +1200

    added torus

--- app.js
+++ app.js
@@ -47,7 +47,9 @@ class App {
             bevelEnabled: false
         }
 
-        const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+        //const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+
+        const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 
         const material = new THREE.MeshStandardMaterial({ color: 0xFF0000 });
 
